5cd05d927afe9afe3a7d5cf73ef1df6a9e3f7566
yomex4life <yomex4life2001@gmail.com>
2021-05-10 13:12:28 +0100

    Completed serverless todo app

---
+++ src/handler/createTodo.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+const uuid = require('uuid');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+module.exports.createTodo = (event, context, callback) =>{
+    const datetime = new Date().toISOString();
+    const data = JSON.parse(event.body);
+
+    if(typeof data.task !== 'string')
+    {
+         console.error('Task is not a string')
+         const response = {
+            statusCode: 400,
+            body: JSON.stringify({"message": "Task is not a string"})
+         }
+    }
+
+    const params = {
+        TableName: 'todos',
+        Item: {
+            id: uuid.v1(),
+            task: data.task,
+            done: false,
+            createdAt: datetime,
+            updatedAt: datetime
+        }
+    }
+
+    dynamoDb.put(params, (error, data) =>{
+        if(error)
+        {
+            console.error(error)
+            callback(new Error(error))
+            return;
+        }
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data.Items)
+        }
+
+        callback(null, response)
+    })
+}
